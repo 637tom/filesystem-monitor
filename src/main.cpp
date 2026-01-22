be0ea8574becda6073b918b22f921e7e648b2701
Tomasz Pływacz <tomek@tomaszs-macbook-pro.home>
2026-01-22 22:35:32 +0100

    Added cmake file

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <string> 
 #include <cstdlib>
 #include <map>
+#include "SQLiteCpp/SQLiteCpp.h"
 namespace fs = std::filesystem;
 
 using FolderMap = std::map<fs::path, std::uintmax_t>;
@@ -18,13 +19,20 @@ std::uintmax_t scan(const fs::path& current_path, FolderMap& sizes) {
                 size += entry.file_size();
             }
         }
-    } catch(const std::filesystem::filesystem_error& e) {
+    } catch(std::exception& e) {
         std::cerr << "Error: " << current_path.string() << ": " << e.what() << "\n";
     }
     sizes[current_path] = size;
     return size;
 }
 
+void processDatabase(const FolderMap& sizes) {
+    try {
+        SQLite::Database db("monitor.db", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+    } catch (std::exception& e) {
+        std::cerr << "Error: " << e.what() << "\n";
+    }
+}
 int main() {
     const char* home_Dir = std::getenv("HOME");
     if(home_Dir == nullptr) {
@@ -34,9 +42,10 @@ int main() {
     fs::path path = fs::path(home_Dir) / "Nsync";
     FolderMap sizes;
     scan(path, sizes);
-    for(const auto& entry : sizes) {
+    /*for(const auto& entry : sizes) {
         std::cout << entry.first.string() << ": " << entry.second << "\n";
-    }
+    }*/
+    processDatabase(sizes);
 
     return 0;
 }
